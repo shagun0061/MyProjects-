390d6956f0c64f0dc851f114e4f2f79e8f923337
shagun0061 <shagun08081999@gmail.com>
2024-05-26 22:15:16 +0530

    todo app with typeScript and drag drop funcnality setup

---
+++ todo app with ts/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     todo app with typescript
+    </>
+  )
+}
+
+export default App
